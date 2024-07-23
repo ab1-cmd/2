20bb3990666b32885632e30ffa375caa299bde02
ab1-cmd <cab031174@gmail.com>
2024-07-23 20:40:28 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,32 @@
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package balance;
+import java.util.Scanner;
+/**
+ *
+ * @author anilk
+ */
+public class Account {
+    int acc,bal;
+    String name;
+    
+    public void read() throws Exception{
+        Scanner s1 = new Scanner (System.in);
+        System.out.println("enter name");
+        name=s1.nextLine();
+        System.out.println("enter account number");
+        acc=s1.nextInt();
+        System.out.println("enter account balance");
+        bal=s1.nextInt();
+        
+    }
+    public void display()throws Exception {
+        System.out.println("ACCOUNT DETAILS");
+        System.out.println("NAME:"+name);
+        System.out.println("ACCOUNT NUMBER:"+acc);
+        System.out.println("ACCOUNT BALANCE:"+bal);
+        
+    }
+}
