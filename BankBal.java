403c362ee692449edcb3b524b24deb56120217e2
ab1-cmd <cab031174@gmail.com>
2024-07-24 10:09:11 +0530

    Update BankBal.java

--- BankBal.java
+++ BankBal.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package balance;
 
-/**
- *
- * @author anilk
- */
 public class BankBal {
     public static void main(String args[]){
         try{
